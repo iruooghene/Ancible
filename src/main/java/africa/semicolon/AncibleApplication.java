36d1ac706baa20917f0598dc6fbb355aa4578748
iruooghene <ebohiruooghene@gmail.com>
2024-11-07 15:55:45 +0100

    first commit

---
+++ src/main/java/africa/semicolon/AncibleApplication.java
@@ -0,0 +1,13 @@
+package africa.semicolon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncibleApplication.class, args);
+	}
+
+}
